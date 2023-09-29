2ae603668e0be98510c2e5d76bc37324e7991fbc
Ray <buttrafay980@gmail.com>
2023-09-29 15:31:23 +0500

    cant chat wito=hout account

--- app/chat/page.jsx
+++ app/chat/page.jsx
@@ -1,10 +1,26 @@
 "use client";
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Chat from '../Components/Chat/chat';
 import { useMessages } from '../Contexts/MessagesContext/MessagesContext';
+import { signIn, useSession } from 'next-auth/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+
+import styles from "./page.module.css";
 
 const page = () => {
+  const { data: session } = useSession();
+  const [ signedIn, setSignedIn ] = useState( false );
+
+  useEffect( () => {
+    if ( session?.user ) {
+      setSignedIn( true );
+    } else {
+      setSignedIn( false );
+    }
+  }, [ session ] );
+
   const [ prompt, setPrompt ] = useState( {
     value: ""
   } );
@@ -12,7 +28,13 @@ const page = () => {
   const { messages, setMessages } = useMessages();
 
   return (
-    <Chat prompt={ prompt } setPrompt={ setPrompt } messages={ messages } setMessages={ setMessages } />
+    <>
+      { signedIn ? (
+        <Chat prompt={ prompt } setPrompt={ setPrompt } messages={ messages } setMessages={ setMessages } />
+      ) : (
+        <button className={ styles[ 'google-btn' ] } onClick={ () => signIn( "google" ) } type="button"><FontAwesomeIcon className={ styles[ 'google-icon' ] } icon={ faGoogle } /><span>Continue with Google</span></button>
+      ) }
+    </>
   );
 };
 
