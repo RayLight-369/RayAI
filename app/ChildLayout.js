0c95f57ce69455799bd5502c06751b88b6790f3f
Ray <buttrafay980@gmail.com>
2023-09-29 18:15:08 +0500

    updating Settings Layout

--- app/ChildLayout.js
+++ app/ChildLayout.js
@@ -28,10 +28,10 @@ const ChildLayout = ( { children } ) => {
 
         </div>
         <div className={ styles[ "links" ] }>
-          <Link href={ "/chat" }><FontAwesomeIcon className={ styles[ 'link-icons' ] } icon={ faMessage } />&nbsp;&nbsp;&nbsp;Chats</Link>
-          <Link href={ "/chat" }><FontAwesomeIcon className={ styles[ 'link-icons' ] } icon={ faMagnifyingGlass } />&nbsp;&nbsp;&nbsp;Search</Link>
-          <Link href={ "/chat" }><FontAwesomeIcon className={ styles[ 'link-icons' ] } icon={ faEnvelope } />&nbsp;&nbsp;&nbsp;Support</Link>
-          <Link href={ "/chat/settings" }><FontAwesomeIcon className={ styles[ 'link-icons' ] } icon={ faGears } />&nbsp;&nbsp;&nbsp;Settings</Link>
+          <Link href={ "/" }><FontAwesomeIcon className={ styles[ 'link-icons' ] } icon={ faMessage } />&nbsp;&nbsp;&nbsp;Chats</Link>
+          <Link href={ "/" }><FontAwesomeIcon className={ styles[ 'link-icons' ] } icon={ faMagnifyingGlass } />&nbsp;&nbsp;&nbsp;Search</Link>
+          <Link href={ "/" }><FontAwesomeIcon className={ styles[ 'link-icons' ] } icon={ faEnvelope } />&nbsp;&nbsp;&nbsp;Support</Link>
+          <Link href={ "/settings" }><FontAwesomeIcon className={ styles[ 'link-icons' ] } icon={ faGears } />&nbsp;&nbsp;&nbsp;Settings</Link>
         </div>
         <div className={ styles[ 'build-info' ] }>
           <div className={ styles[ 'version' ] }>
