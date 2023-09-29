aaaaa0e2e9806945bb60f9799a7ba1c111875561
Ray <buttrafay980@gmail.com>
2023-09-29 15:32:12 +0500

    cant chat without account

--- app/chat/page.jsx
+++ app/chat/page.jsx
@@ -7,12 +7,14 @@ import { signIn, useSession } from 'next-auth/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
+import styles from "./page.module.css";
+
 const page = () => {
   const { data: session } = useSession();
   const [ signedIn, setSignedIn ] = useState( false );
 
   useEffect( () => {
-    if ( session.user ) {
+    if ( session?.user ) {
       setSignedIn( true );
     } else {
       setSignedIn( false );
@@ -30,7 +32,7 @@ const page = () => {
       { signedIn ? (
         <Chat prompt={ prompt } setPrompt={ setPrompt } messages={ messages } setMessages={ setMessages } />
       ) : (
-        <button onClick={ () => signIn( "google" ) } type="button"><FontAwesomeIcon className={ styles[ 'google-icon' ] } icon={ faGoogle } /><span>Continue with Google</span></button>
+        <button className={ styles[ 'google-btn' ] } onClick={ () => signIn( "google" ) } type="button"><FontAwesomeIcon className={ styles[ 'google-icon' ] } icon={ faGoogle } /><span>Continue with Google</span></button>
       ) }
     </>
   );
