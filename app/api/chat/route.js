8af0c612a5e1882365b122daf70907415b8f04cc
Ray <buttrafay980@gmail.com>
2023-10-04 19:05:21 +0500

    update

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -8,12 +8,17 @@ const openai = new OpenAI( {
 export const runtime = 'edge';
 
 export async function POST ( req ) {
-  const { messages } = await req.json();
-  const response = await openai.chat.completions.create( {
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages,
-  } );
-  const stream = OpenAIStream( response );
-  return new StreamingTextResponse( stream );
+  try {
+    const { messages } = await req.json();
+    const response = await openai.chat.completions.create( {
+      model: 'gpt-3.5-turbo',
+      stream: true,
+      messages,
+    } );
+    const stream = OpenAIStream( response );
+    return new StreamingTextResponse( stream );
+  } catch ( e ) {
+    console.log( e );
+
+  }
 }
