b5e5a687deee0dbe2bcfc8bfc18b8ac0fccadbdf
RayLight-369 <136804809+RayLight-369@users.noreply.github.com>
2023-10-06 22:11:54 +0500

    Update route.js

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -42,11 +42,11 @@ export async function POST ( req ) {
     model: 'OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5',
     inputs: experimental_buildOpenAssistantPrompt( messages ),
     parameters: {
-      max_new_tokens: 2000,
+      max_new_tokens: 1000,
       // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
       typical_p: 0.2,
       repetition_penalty: 1,
-      truncate: 2000,
+      truncate: 1000,
       return_full_text: false,
     }
   } );
